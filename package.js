b3cd7d971fb78cfa419dd736be3ced0d2e8c17df
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-06-27 16:41:04 -0500

    update Stripe lib to 4.7.0

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   name: "jeremy:stripe",
   summary: "Stripe.js, Stripe Checkout, and Stripe for Node (async/sync) wrapped up for Meteor.",
-  version: "1.4.1",
+  version: "1.5.0",
   git: "https://github.com/jshimko/meteor-stripe.git"
 });
 
 Package.on_use(function (api) {
 
   Npm.depends({
-    "stripe": "4.5.0"
+    "stripe": "4.7.0"
   });
 
   api.use([
-    "ecmascript@0.4.3",
-    "underscore@1.0.8",
-    "reactive-var@1.0.9"
+    "ecmascript@0.4.6",
+    "underscore@1.0.9",
+    "reactive-var@1.0.10"
   ]);
 
   api.addFiles("stripe_client.js", "client");
